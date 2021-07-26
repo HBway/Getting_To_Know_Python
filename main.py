bc28ac709667730df3640377bb9ced06080f83dd
HunterBroadaway <>
2021-07-26 17:46:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#This program is to learn the syntax of Python
+
+from turtle import Turtle, Screen
+
+#colon is used for type-hinting.
+t: Turtle = Turtle() 
+s: Screen = Screen()
+
+t.forward(150)
+t.right(90)
+t.forward(150)
+t.right(90)
+t.forward(150)
+t.right(90)
+t.forward(150)
